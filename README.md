a151b61e6c51cb6d2adfb9be2e797c65347c868e
serlus <serluscasas@gmail.com>
2020-04-22 09:09:52 -0300

    adicionada linter

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # libpythonpro
 Módulo de estudos para pytools do curso python pro
+
+para instalar:
+
+'''console
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements-dev.
+
+'''
+para conferir qualidade de código:
+
+'''console
+flake8
+'''
--- liblearn/libpythonpro/github_api.py
+++ liblearn/libpythonpro/github_api.py
@@ -1,17 +1,18 @@
 import requests
 
+
 def buscar_avatar(usuario):
     """
     Busca o avatar de um usuário no Github
 
     :para usuarios: str com o nome de usuário no github
-    :return: str com o link do avatar
-    json serve como dicionario e dentro da chave digo o q quero buscar
+    :return: str com o link do avatar json serve como 
+     dicionario e dentro da chave digo o q quero buscar
     """
 
-    url=f'https://api.github.com/users/{usuario}'
+    url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
     return resp.json()['avatar_url']
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     print(buscar_avatar('serlus'))
