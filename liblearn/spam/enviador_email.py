a4e8abcff639dc5eaee74e11f3fdbe4639647f95
serlus <serluscasas@gmail.com>
2020-04-27 18:29:55 -0300

    ultimo teste com erro modulo

--- liblearn/spam/enviador_email.py
+++ liblearn/spam/enviador_email.py
@@ -1,3 +1,9 @@
 class Enviador:
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        return 'serluscasas@gmail.com'
+    def enviar(self, destinatario, remetente, assunto, corpo):
+        if '@' not in remetente:
+            raise EmailInvalido(f'Email de remetente inválido: {remetente}')
+        return remetente
+
+
+class EmailInvalido(Exception):
+    pass
