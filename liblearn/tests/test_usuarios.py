246452fb878ff6f6c221dd1e09ca962bded606f5
serlus <serluscasas@gmail.com>
2020-05-10 13:06:06 -0300

    implementado fixture

--- liblearn/tests/test_usuarios.py
+++ liblearn/tests/test_usuarios.py
@@ -1,25 +1,30 @@
+import pytest
 from liblearn.spam.db import Conexao
 from liblearn.spam.models import Usuario
 
+@pytest.fixture
+def conexao():
+    # Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    # Tear down
+    conexao_obj.fechar()
 
-def test_salvar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Renzo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuarios():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
