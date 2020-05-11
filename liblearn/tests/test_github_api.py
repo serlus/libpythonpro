418273cf3212711556edc4a94f41dc033b2d5cb0
serlus <serluscasas@gmail.com>
2020-05-11 08:12:28 -0300

    implementado mock

---
+++ liblearn/tests/test_github_api.py
@@ -0,0 +1,14 @@
+from unittest.mock import Mock
+from liblearn import github_api
+
+
+def test_buscar_avatar():
+    resp_mock = Mock()
+    resp_mock.json.return_value = {
+        'login':	'serlus', 'id':	63012359,
+        'avatar_url':	'https://avatars0.githubusercontent.com/u/63012359?v=4',
+    }
+    github_api.requests.get = Mock(return_value=resp_mock)
+    url = github_api.buscar_avatar('serlus')
+    assert 'https://avatars0.githubusercontent.com/u/63012359?v=4' == url
+    
