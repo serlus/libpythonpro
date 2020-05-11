e0b2a16ef5d41079aa4beca63b1bc3265ffece34
serlus <serluscasas@gmail.com>
2020-05-10 23:12:00 -0300

    import mock

--- liblearn/tests/test_envio_para_base_de_usuarios.py
+++ liblearn/tests/test_envio_para_base_de_usuarios.py
@@ -1,7 +1,21 @@
 from liblearn.spam.models import Usuario
 import pytest
+from unittest.mock import Mock
 from liblearn.spam.enviador_email import Enviador
 from liblearn.spam.main import EnviadorDeSpam
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
 
 @pytest.mark.parametrize(
     'usuarios',
@@ -18,42 +32,30 @@ from liblearn.spam.main import EnviadorDeSpam
 def test_qd_de_spam(sessao, usuarios):
     for usuario in usuarios:
         sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'serlusmc@yahoo.com.br',
         'Curso python Pro',
         'Confirma os módulos fantásticos'
     )
-    assert len(usuarios) == enviador.qtd_email_enviados
+    assert len(usuarios) == enviador.enviar.call_count
 
-
-class EnviadorMock(Enviador):
-
-    def __init__(self):
-        super().__init__()
-        self.qtd_email_enviados = 0
-        self.parametros_de_envio = None
-
-    def enviar(self, remetente, destinatario, assunto, corpo):
-        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
-        self.qtd_email_enviados += 1
 
 
 def test_parametros_de_spam(sessao):
     usuario = Usuario(nome='Sergio', email='serluscasas@gmail.com')
     sessao.salvar(usuario)
-    enviador = EnviadorMock()
+    enviador = Mock()
     enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'serlusmc@yahoo.com.br',
         'Curso python Pro',
         'Confirma os módulos fantásticos'
     )
-    assert enviador.parametros_de_envio == (
+    enviador.enviar.assert_called_once_with(
         'serlusmc@yahoo.com.br',
         'serluscasas@gmail.com',
         'Curso python Pro',
         'Confirma os módulos fantásticos'
     )
-    
